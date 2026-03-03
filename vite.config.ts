e218c92b592f48c0b7b63b3c5480b7f0f0a8c7e3
Max <max@mail.ru>
2026-03-03 23:45:44 +1000

    новый сайт-портфолио

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
@@ -15,7 +13,6 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
